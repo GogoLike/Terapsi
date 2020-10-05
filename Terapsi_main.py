690ee9b872923b1a02eb54c305947880799cfdcc
GogoLike <iekalayda@gmail.com>
2020-10-05 14:50:26 +0300

    Create MainWindow in QtDesigner

--- Terapsi_main.py
+++ Terapsi_main.py
@@ -1,1 +1,8 @@
-print("Hello, world!")
+from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication
+
+app = QApplication([])
+window = uic.loadUi("Terapsi_Window.ui")
+window.show()
+
+exit(app.exec_())
