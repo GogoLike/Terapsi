5143c7c793feeeeffa60a74b61c073419c19732c
GogoLike <iekalayda@gmail.com>
2020-10-05 15:13:50 +0300

    Create Terapsi.db

--- Terapsi_main.py
+++ Terapsi_main.py
@@ -1,8 +1,35 @@
 from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication
+import sqlite3
+
+
+# =====================================================================================================================
+
+class TerapsiDB:
+    def __init__(self):
+        self.conn = sqlite3.connect("""Terapsi.db""")
+        self.cursor = self.conn.cursor()
+
+
+# =====================================================================================================================
+
+def print_hi():
+    print("Hi!")
+
+
+# =====================================================================================================================
+
+
+# =====================================================================================================================
+
+print("""Terapsi has started""")
+
+database = TerapsiDB()
 
 app = QApplication([])
 window = uic.loadUi("Terapsi_Window.ui")
 window.show()
+
+window.actionOpen.triggered.connect(print_hi)
 
 exit(app.exec_())
